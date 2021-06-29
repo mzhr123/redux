cff9e4b39801bbcc2e1bd4174a264d5daa79ef8d
mzhr123 <78639441+mohammedsaam@users.noreply.github.com>
2021-06-30 00:25:13 +0530

    redux

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 
 import './App.css';
 import {useSelector,useDispatch} from "react-redux";
-// import { increment,decrement } from "./state/store/Store";
 import {increment,decrement} from './state/action/action'
 import Timer from "./components/Timer"
 
 function App() {
-  // const firstState=useSelector((state)=>state.changeTheNumber);
+  const firstState=useSelector((state)=>state.changeTheNumber);
   const dispatch=useDispatch();
-  // const selector=useSelector();
   return (
     <div className="App">
       <h1>increment/decrement using redux</h1>
       
       <button onClick={()=>dispatch(increment())}>+</button>
-      <h2>{useSelector((state)=>state.changeTheNumber)}</h2>
+      <h2>{firstState}</h2>
       <button onClick={()=>dispatch(decrement())}>-</button>
 
       <div>
